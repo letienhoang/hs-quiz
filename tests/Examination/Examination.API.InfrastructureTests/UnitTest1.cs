8eed4170df345944829a9785a039ec44dcc474f9
Holwn <ltienhoang2@gmail.com>
2025-02-11 10:04:41 +0700

    feat: solution struture

---
+++ tests/Examination/Examination.API.InfrastructureTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Examination.API.InfrastructureTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Examination/Examination.InfrastructureTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Examination.InfrastructureTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
